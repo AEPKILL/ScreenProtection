000811f49f7ecafb763fc8137623ca335df2c528
AEPKILL <A@AEPKILL.COM>
2016-10-01 05:58:46 +0800

    修改配置文件策略

--- ScreenProtection/main.cpp
+++ ScreenProtection/main.cpp
@@ -15,15 +15,77 @@
 // 引入 GdiPlus
 #pragma comment(lib,"gdiplus.lib")
 
+
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+TCHAR iniFilePath[MAX_PATH];
+
+LPTSTR GetArgument(LPTSTR commandStr, int index) 
+{
+	static TCHAR buffer[MAX_PATH];
+	bool quoteFlag = false;
+	int commandLineLength = lstrlen(commandStr), current = 0, bufferIndex = 0;
+	buffer[0] = '\0';
+	for (int i = 0; i < commandLineLength; i++)
+	{
+		TCHAR ch = commandStr[i];
+		if (ch == '\"')
+		{
+			quoteFlag = !quoteFlag;
+		}
+		else if (ch == ' ' && !quoteFlag)
+		{
+			if (index == current)
+			{
+				break;
+			}
+			else
+			{
+				current++;
+			}
+			// 跳过连续空格
+			while (i<commandLineLength - 1 && commandStr[i+1] == ' ')
+			{
+				i++;
+			}
+		}
+		else
+		{
+			if (index == current)
+			{
+				buffer[bufferIndex++] = commandStr[i];
+				buffer[bufferIndex] = '\0';
+			}
+		}
+	}
+
+	return buffer;
+}
+
 int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
 {
 	static TCHAR szAppName[] = TEXT("AEPKILL");
 	HWND            hwnd;
 	MSG            msg;
 	WNDCLASS    wndclass;
-
-
+	LPTSTR commandLine = GetCommandLine();
+	LPTSTR arg = GetArgument(commandLine , 1);
+	int iniFileLength = 0;
+	lstrcat(iniFilePath,GetArgument(commandLine,0));
+	iniFileLength = lstrlen(iniFilePath);
+	iniFileLength = iniFileLength - 4;
+	iniFilePath[iniFileLength] = '\0';
+	lstrcat(iniFilePath, TEXT(".ini"));
+	if ( arg[0] == '/') 
+	{
+		if (arg[1] == 'p')
+		{
+			return 0;
+		}
+		if (arg[1] == 'c')
+		{
+			MessageBox(NULL, TEXT("不支持设置"), TEXT("Error"), MB_ICONERROR);
+		}
+	}
 	wndclass.style = CS_HREDRAW | CS_VREDRAW;
 	wndclass.lpfnWndProc = WndProc;
 	wndclass.cbClsExtra = 0;
@@ -99,7 +161,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		memGraphics = new Graphics(bitmap);
 		// 初始化屏保绘图类
 		screenProtection = new ScreenProtection(cxScreen, cyScreen);
-		screenProtection->Init(TEXT("D:/ScreenProtection.ini"));
+		screenProtection->Init(iniFilePath);
 		// 创建定时器
 		SetTimer(hwnd, ID_TIMER, 100, NULL);
 		return 0;
